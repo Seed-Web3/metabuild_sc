2598c0c8ed6cdd2773358429699d2deb39f3a418
Sordgom <aymaneigmiden@gmail.com>
2022-10-17 21:57:41 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,69 @@
+## Prerequisites
+
+* [Node.js](/develop/prerequisites#nodejs)
+* [NEAR Wallet Account](wallet.testnet.near.org)
+* [NEAR-CLI](https://docs.near.org/tools/near-cli#setup)
+* [yarn](https://classic.yarnpkg.com/en/docs/install#mac-stable)
+
+
+## Mint An NFT
+
+Once you've created your near wallet go ahead and login to your wallet with your cli and follow the on-screen prompts
+
+```=bash
+near login
+```
+
+Once your logged in you have to deploy the contract. Make a subaccount with the name of your choosing 
+
+```=bash 
+near create-account nft-example.your-account.testnet --masterAccount your-account.testnet --initialBalance 10
+```
+
+After you've created your sub account deploy the contract to that sub account, set this variable to your sub account name
+
+```=bash
+NFT_CONTRACT_ID=nft-example.your-account.testnet
+
+MAIN_ACCOUNT=your-account.testnet
+```
+
+Verify your new variable has the correct value
+```=bash
+echo $NFT_CONTRACT_ID
+
+echo $MAIN_ACCOUNT
+```
+
+
+### Deploy Your Contract
+```=bash
+near deploy --accountId $NFT_CONTRACT_ID --wasmFile build/glory.wasm
+```
+
+### Initialize Your Contract 
+
+```=bash
+near call $NFT_CONTRACT_ID init '{"owner_id": "'$NFT_CONTRACT_ID'"}' --accountId $NFT_CONTRACT_ID
+```
+
+### View Contracts Meta Data
+
+```=bash
+near view $NFT_CONTRACT_ID nft_metadata
+```
+### Minting Token
+
+```bash=
+near call $NFT_CONTRACT_ID nft_mint '{"token_id": "token-1", "metadata": {"title": "TEST-NFT", "description": "This is a drill", "media": "https://media.giphy.com/media/6SZ5iwN70lJyOdLZZH/giphy.gif"}, "receiver_id": "'$NFT_CONTRACT_ID'"}' --accountId $NFT_CONTRACT_ID --amount 0.1
+```
+
+After you've minted the token go to wallet.testnet.near.org to `your-account.testnet` and look in the collections tab and check out your new sample NFT! 
+
+## View NFT Information
+
+After you've minted your NFT you can make a view call to get a response containing the `token_id` `owner_id` and the `metadata`
+
+```bash=
+near view $NFT_CONTRACT_ID nft_token '{"token_id": "token-1"}'
+```
---
+++ src/glory-contract/index.ts
@@ -0,0 +1,100 @@
+
+import { NearContract, NearBindgen, near, call, view, LookupMap, UnorderedMap, Vector, UnorderedSet } from 'near-sdk-js'
+import { NFTContractMetadata, Token, TokenMetadata, internalNftMetadata } from './metadata';
+import { internalMint } from './mint';
+import { internalNftTokens, internalSupplyForOwner, internalTokensForOwner, internalTotalSupply } from './enumeration';
+import { internalNftToken } from './nft_core';
+
+/// This spec can be treated like a version of the standard.
+export const NFT_METADATA_SPEC = "nft-1.0.0";
+
+/// This is the name of the NFT standard we're using
+export const NFT_STANDARD_NAME = "nep171";
+
+@NearBindgen
+export class Contract extends NearContract {
+    owner_id: string;
+    tokensPerOwner: LookupMap;
+    tokensById: LookupMap;
+    tokenMetadataById: UnorderedMap;
+    metadata: NFTContractMetadata;
+
+    /*
+        initialization function (can only be called once).
+        this initializes the contract with metadata that was passed in and
+        the owner_id. 
+    */
+    constructor({
+        owner_id, 
+        metadata = {
+            spec: "glory-badge-1.0.0",
+            name: "Seed Glory Badge",
+            symbol: "SGB"
+        } 
+    }) {
+        super()
+        this.owner_id = owner_id;
+        this.tokensPerOwner = new LookupMap("tokensPerOwner");
+        this.tokensById = new LookupMap("tokensById");
+        this.tokenMetadataById = new UnorderedMap("tokenMetadataById");
+        this.metadata = metadata;
+    }
+
+    default() {
+        return new Contract({owner_id: ''})
+    }
+
+    /*
+        MINT
+        TODO: can only mint once
+    */
+    @call
+    nft_mint({ token_id, metadata, receiver_id, perpetual_royalties }) {
+        return internalMint({ contract: this, tokenId: token_id, metadata: metadata, receiverId: receiver_id, perpetualRoyalties: perpetual_royalties });
+    }
+
+    /*
+        CORE
+    */
+    @view
+    //get the information for a specific token ID
+    nft_token({ token_id }) {
+        return internalNftToken({ contract: this, tokenId: token_id });
+    }
+
+    /*
+        ENUMERATION
+    */
+    @view
+    //Query for the total supply of NFTs on the contract
+    nft_total_supply() {
+        return internalTotalSupply({ contract: this });
+    }
+
+    @view
+    //Query for nft tokens on the contract regardless of the owner using pagination
+    nft_tokens({ from_index, limit }) {
+        return internalNftTokens({ contract: this, fromIndex: from_index, limit: limit });
+    }
+
+    @view
+    //get the total supply of NFTs for a given owner
+    nft_tokens_for_owner({ account_id, from_index, limit }) {
+        return internalTokensForOwner({ contract: this, accountId: account_id, fromIndex: from_index, limit: limit });
+    }
+
+    @view
+    //Query for all the tokens for an owner
+    nft_supply_for_owner({ account_id }) {
+        return internalSupplyForOwner({ contract: this, accountId: account_id });
+    }
+
+    /*
+        METADATA
+    */
+    @view
+    //Query for all the tokens for an owner
+    nft_metadata() {
+        return internalNftMetadata({ contract: this });
+    }
+}
